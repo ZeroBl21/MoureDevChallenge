6ed4612af09d6de75d53bdd879eff01bbbf2d0bf
Darwin Perdomo <ZeroBl@hotmail.com>
2022-05-13 20:03:01 -0400

    Javascript: wordCount (Completed)

---
+++ JS/Challenge7/wordCount.js
@@ -0,0 +1,43 @@
+/*
+ * Reto #7
+ * CONTANDO PALABRAS
+ * Fecha publicación enunciado: 14/02/22
+ * Fecha publicación resolución: 21/02/22
+ * Dificultad: MEDIA
+ *
+ * Enunciado: Crea un programa que cuente cuantas veces se repite cada palabra y que muestre el recuento final de todas ellas.
+ * - Los signos de puntuación no forman parte de la palabra.
+ * - Una palabra es la misma aunque aparezca en mayúsculas y minúsculas.
+ * - No se pueden utilizar funciones propias del lenguaje que lo resuelvan automáticamente.
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda la acomunidad.
+ * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+ * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+ * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+ *
+ */
+
+const wordCount = (phrase) => {
+    const words = phrase.toLowerCase().split(" ");
+    let obj = {};
+
+    for (const word of words) {
+        obj[word] = isNaN(obj[word]) ? 1 : obj[word] + 1;
+    }
+
+    const sortable = Object.fromEntries(
+        Object.entries(obj).sort(([, a], [, b]) => a - b)
+    );
+
+    return sortable;
+};
+
+// This is the synopsis of "The Way of the Kings" by Brandon Sanderson, a Really nice book.
+//
+//
+const text = `
+Roshar is a world of stone and storms. Uncanny tempests of incredible power sweep across the rocky terrain so frequently that they have shaped ecology and civilization alike. Animals hide in shells, trees pull in branches, and grass retracts into the soilless ground. Cities are built only where the topography offers shelter. It has been centuries since the fall of the ten consecrated orders known as the Knights Radiant, but their Shardblades and Shardplate remain: mystical swords and suits of armor that transform ordinary men into near-invincible warriors. Men trade kingdoms for Shardblades. Wars were fought for them, and won by them.
+`;
+
+console.log(wordCount(text));
